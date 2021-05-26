65e8a52adb54808e672c9c914a1e05a92608f381
haiderra <84810028+haiderra@users.noreply.github.com>
2021-05-26 17:43:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/************************************************************
+* CMSC 255 - Lab 01
+*************************************************************
+* Raza Haider
+* 05/25/2021
+* CMSC-255-C01-SU2021
+************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Raza Haider");
+		System.out.println("Lab 01 - Introduction to the Course");
+		System.out.println("This lab helps to get started with Java. The goal is to run a program on Java to ensure it is properly installed and loaded on my computer.");
+		System.out.println("05/25/2021");
+		System.out.println("CMSC-255-C01-SU2021");
+		System.out.println();
+	}
+}
